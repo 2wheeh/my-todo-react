d744572c7404648550f20838ed73da02b24e5666
wnhlee <2wheeh@gmail.com>
2023-04-23 05:52:37 +0900

    ✏️ fixup

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const DarkModeContext = createContext();
 
@@ -9,6 +9,16 @@ export const DarkModeProvider = ({ children }) => {
     setDarkMode(!darkMode);
     updateDarkMode(!darkMode);
   };
+
+  useEffect(() => {
+    const isDark =
+      localStorage.them === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+    setDarkMode(isDark); // update state
+    updateDarkMode(isDark); // apply mode changed
+  }, []);
 
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
@@ -22,7 +32,9 @@ export const useDarkMode = () => useContext(DarkModeContext);
 const updateDarkMode = darkMode => {
   if (darkMode) {
     document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
   }
 };
