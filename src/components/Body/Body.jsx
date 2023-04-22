55135a1f6ce4963500ecd1eb8cbedb0d2d27db85
wnhlee <2wheeh@gmail.com>
2023-04-23 02:43:33 +0900

    🔨 store todos in localStorage

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoAdd from "../TodoAdd/TodoAdd";
 import TodoList from "../TodoList/TodoList";
 
 export default function Body() {
-  const [todos, setTodos] = useState(dummy);
+  const [todos, setTodos] = useState(readTodos);
 
   const handleAdd = todo => {
     setTodos([...todos, todo]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <section>
@@ -17,8 +21,15 @@ export default function Body() {
   );
 }
 
-const dummy = [
-  { id: "1", desc: "잠자기", status: "active" },
-  { id: "2", desc: "숨쉬기", status: "active" },
-  { id: "3", desc: "눈뜨기", status: "completed" },
-];
+// const dummy = [
+//   { id: "1", desc: "잠자기", status: "active" },
+//   { id: "2", desc: "숨쉬기", status: "active" },
+//   { id: "3", desc: "눈뜨기", status: "completed" },
+// ];
+
+/* read todos from localStorage.
+ * If there is not todos, it returns an empty array */
+const readTodos = () => {
+  const todos = localStorage.getItem("todos");
+  return todos ? JSON.parse(todos) : [];
+};
